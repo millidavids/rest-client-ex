ebe6fed9ff54c361134c2de04192c464262fe6b3
millidavids <david.yurek@realpage.com>
2016-03-18 11:17:41 -0400

    finish

--- api-test.php
+++ api-test.php
@@ -5,7 +5,38 @@ require __DIR__.'/vendor/autoload.php';
 $client = new \Delatbabel\ApiSecurity\Helpers\Client();
 
 $client->setPrivateKey(__DIR__.'/private_key');
-$params = array();
 
-$response = file_get_contents("http://homestead.app/company?sig=".$client->createHMAC($params));
+// Index
+$params = array();
+$hmac = $client->createHMAC($params);
+$response = file_get_contents("http://homestead.app/company?hmac=".$params['hmac']."&cnonce=".$params['cnonce']);
 echo $response;
+
+// Show
+$params = array();
+$hmac = $client->createHMAC($params);
+$response = file_get_contents("http://homestead.app/company/1?hmac=".$params['hmac']."&cnonce=".$params['cnonce']);
+echo $response;
+
+// Store
+$params = array('name' => 'lol', 'url' => 'lol.com');
+$hmac = $client->createHMAC($params);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'http://homestead.app/company');
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'name='.$params['name'].'&url='.$params['url'].'&hmac='.$params['hmac'].'&cnonce='.$params['cnonce']);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+echo $response;
+
+// Update
+$params = array('name' => 'lol', 'url' => 'lol.com');
+$hmac = $client->createHMAC($params);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'http://homestead.app/company/1');
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'name='.$params['name'].'&url='.$params['url'].'&hmac='.$params['hmac'].'&cnonce='.$params['cnonce']);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+echo $response;
+
